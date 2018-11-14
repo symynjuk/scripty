2370131486af601a30335f7b9183bffd66780dd5
VadimPoltoratskiy <vadjan87heavymetall@gmail.com>
2018-11-14 16:32:21 +0200

    14.11.2018 done sceleton

---
+++ backend/src/main/java/co/inventorsoft/academy/AcademyApplication.java
@@ -0,0 +1,12 @@
+package co.inventorsoft.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyApplication.class, args);
+    }
+}
