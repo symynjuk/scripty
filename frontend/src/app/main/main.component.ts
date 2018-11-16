9ba8caa530c72d1f4ce2e2a2394d584330e5de91
Diana <dl11021997@gmail.com>
2018-11-16 16:29:48 +0200

    delete send1 folder

---
+++ frontend/src/app/main/main.component.ts
@@ -0,0 +1,62 @@
+import { Component, ViewChild, ElementRef, Input } from '@angular/core';
+
+import { fromEvent } from 'rxjs'
+import { debounceTime } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+
+  @Input() parentData
+
+  iframe = ""
+
+  @ViewChild('htmlPanel') htmlPanel: ElementRef;
+  @ViewChild('iframePanel') iframePanel: ElementRef;
+  @ViewChild('cssPanel') cssPanel: ElementRef;
+  @ViewChild('jsPanel') jsPanel: ElementRef;
+
+  htmlPanelValue = ""; 
+  cssPanelValue = ""; 
+  jsPanelValue = ""
+
+  getValueFromArea(panel, where) {
+    fromEvent(panel.nativeElement, "keyup")
+      .pipe(
+        map((event: any) => { return event.target.value; })
+      ).pipe(
+      debounceTime(500)
+      )
+      .subscribe(ch => {
+        if (where === "html") {
+          this.htmlPanelValue = "<html>" + ch + "</html>"
+        } else if (where === "style") {
+          this.cssPanelValue = "<style>" + ch + "</style>"
+        } else if (where === "script") {
+          this.jsPanelValue = "<script>" + ch + "</script>"
+        }
+
+        let doc = this.iframePanel.nativeElement.contentDocument
+
+        doc.open();
+        doc.write(this.htmlPanelValue);
+        doc.write(this.cssPanelValue);
+        doc.write(this.jsPanelValue);
+        doc.close();
+      })
+  }
+
+  ngOnChanges() {
+    this.getValueFromArea(this.htmlPanel, "html")
+    this.getValueFromArea(this.cssPanel, "style")
+    this.getValueFromArea(this.jsPanel, "script")
+
+    if (this.parentData === true) {
+      this.iframe = "iframe-show"
+    }
+  }
+}
