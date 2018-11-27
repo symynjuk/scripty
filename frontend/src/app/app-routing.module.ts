436ae7d53cadaf56d2f64a51471f23935d9bcb23
Dmytro_Rusnak <rysuy7@gmail.com>
2018-11-27 14:41:13 +0200

    Pulled Diana's branch

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdminPageComponent } from './components/layouts/admin/admin-page/admin-page.component';
+import {HomeComponent} from './components/layouts/home/home.component';
 import {ProjectComponent} from './project/project.component';
 
-const routes: Routes = [{
-    path: '',
-    component: ProjectComponent
-}];
+const routes: Routes = [
+  // { path: '', component:  },
+	{ path: '', 	component: ProjectComponent },
+  { path: 'admin', component: AdminPageComponent}
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminPageComponent } from './components/layouts/admin/admin-page/admin-page.component';
+import { AdminFooterComponent } from './components/layouts/admin/admin-page/footer/footer.component';
+import { AdminHeaderComponent} from './components/layouts/admin/admin-page/header/header.component';
+import { AdminSidebarComponent} from './components/layouts/admin/admin-page/sidebar/sidebar.component';
 import { ProjectComponent } from './project/project.component';
 import { FooterComponent } from './project/footer/footer.component';
 import {MainComponent} from './project/main/main.component';
@@ -14,6 +19,11 @@ import {HeaderComponent} from './project/header/header.component';
     AppComponent,
     SidebarComponent,
     HeaderComponent,
+    MainComponent,
+    AdminPageComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    AdminSidebarComponent,
     MainComponent,
     ProjectComponent,
     FooterComponent
