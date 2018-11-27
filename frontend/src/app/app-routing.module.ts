0527188650cc0fb78235733ca7535ac946089835
Dmytro_Rusnak <rysuy7@gmail.com>
2018-11-27 14:50:44 +0200

    Serucing basic layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { AdminPageComponent } from './components/layouts/admin/admin-page/admin-page.component';
+import {HomeComponent} from './components/layouts/home/home.component';
 
-const routes: Routes = [{
-  path: '',
-  component: AppComponent
-}];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,19 +6,33 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminPageComponent } from './components/layouts/admin/admin-page/admin-page.component';
+import { AdminFooterComponent } from './components/layouts/admin/admin-page/footer/footer.component';
+import { AdminHeaderComponent} from './components/layouts/admin/admin-page/header/header.component';
+import { AdminSidebarComponent} from './components/layouts/admin/admin-page/sidebar/sidebar.component';
+import { HomeComponent } from './components/layouts/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    AdminPageComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    AdminSidebarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
