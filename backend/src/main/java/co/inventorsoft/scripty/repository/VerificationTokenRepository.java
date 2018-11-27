09b85f46ea8263499625b090a6cb32a0644b98b7
symynjuk <mike.thats.my.name@gmail.com>
2018-11-28 00:17:29 +0200

    corrected activate user account endpoint, registration endpoint, regenerate verification link 28.11.2018

--- backend/src/main/java/co/inventorsoft/scripty/repository/VerificationTokenRepository.java
+++ backend/src/main/java/co/inventorsoft/scripty/repository/VerificationTokenRepository.java
@@ -1,10 +1,10 @@
 package co.inventorsoft.scripty.repository;
-
 import co.inventorsoft.scripty.model.entity.User;
 import co.inventorsoft.scripty.model.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    VerificationToken findByToken(String token);
-    VerificationToken findByUser(User user);
+    Optional<VerificationToken> findByToken(String token);
+    Optional<VerificationToken> findByUser(User user);
 }
