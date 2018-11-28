95412462987e537597b7e31333922b3123897077
Dmytro Rusnak <rysuy7@gmail.com>
2018-11-28 13:19:15 +0200

    Continuing work on a layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AdminPageComponent } from './components/layouts/admin/admin-page/admin-page.component';
-import {HomeComponent} from './components/layouts/home/home.component';
+import { AdminPageComponent } from './layouts/admin/admin-page/admin-page.component';
+import {HomeComponent} from './layouts/home/home.component';
 import {ProjectComponent} from './project/project.component';
 
 const routes: Routes = [
