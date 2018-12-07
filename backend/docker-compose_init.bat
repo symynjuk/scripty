65d346f4dfb3fa6ed18588d4457c484bd4f75685
vadim poltoratskiy <vadjan87heavymetall@gmail.com>
2018-12-07 21:23:47 +0200

    fixed for fully compliant windows batch scripts

--- backend/docker-compose_init.bat
+++ backend/docker-compose_init.bat
@@ -1,3 +1,8 @@
+set path_1=%cd:\=/%
+set path_1=/%path_1:C:=c%
+echo %path_1%
+mkdir C:\maven-repo
 docker-machine start default
-docker run -it --rm -v %cd%:/usr/src/mymaven -v %UserProfile%/.m2:/root/.m2 -w /usr/src/mymaven maven mvn clean package -Dmaven.test.skip=true
-call docker-compose up --build
+docker image prune -f
+docker run -it --rm -v "%path_1%":/usr/src/mymaven -v /c/maven-repo:/root/.m2 -w /usr/src/mymaven maven mvn clean package -Dmaven.test.skip=true
+docker-compose up --build
