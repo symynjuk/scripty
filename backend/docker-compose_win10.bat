90b5c1a631d8f57c388b1d6fe7299b3aeaba299c
vadim poltoratskiy <vadjan87heavymetall@gmail.com>
2018-12-07 16:22:54 +0200

    done script fix

--- backend/docker-compose_win10.bat
+++ backend/docker-compose_win10.bat
@@ -1,2 +1,3 @@
-docker run -it --rm -v %cd%:/usr/src/mymaven -v %UserProfile%/.m2:/root/.m2 -w /usr/src/mymaven maven mvn clean package -Dmaven.test.skip=true
+mkdir %UserProfile%\.m2
+docker run -it --rm -v %cd%:/usr/src/mymaven -v %UserProfile%\ªª.m2:/root/.m2 -w /usr/src/mymaven maven mvn clean package -Dmaven.test.skip=true
 docker-compose up --build
